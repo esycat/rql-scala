4226cd10797138bb09fede466fad4e4e529196cd
Eugene Janusov <esycat@gmail.com>
2013-11-18 00:58:01 +1100

    Tooling.

---
+++ src/main/scala/com/esyfur/rql/util/package.scala
@@ -0,0 +1,24 @@
+package com.esyfur.rql
+
+import java.util.TimeZone
+import org.joda.time.{DateTime, DateTimeZone}
+
+package object util {
+
+    private[rql] def printCursor(cursor: Cursor): Unit = {
+        if (cursor.hasNext) println(cursor.next())
+        else println("Cursor empty")
+    }
+
+    private[rql] def getTzOffset(timeZone: TimeZone): String = {
+        val tpl = "%s%02d:%02d"
+        val offset = timeZone.getRawOffset()
+        val sign = if (offset < 0) "-" else "+"
+        tpl.format(sign, Math.abs(offset) / 3600000, Math.abs(offset) / 60000 % 60)
+    }
+
+    private[rql] def getTzOffset(timeZone: DateTimeZone): String = getTzOffset(timeZone.toTimeZone)
+
+    private[rql] def currentTime: DateTime = new DateTime(DateTimeZone.UTC)
+
+}
--- src/test/scala/com/esyfur/rql/test/UnitSpec.scala
+++ src/test/scala/com/esyfur/rql/test/UnitSpec.scala
@@ -1,15 +1,20 @@
 package com.esyfur.rql.test
 
 import org.scalatest.{FunSpec, Matchers, BeforeAndAfterAll, OptionValues, Inside}
+
 import com.esyfur.rql.Cursor
 
 abstract class UnitSpec extends FunSpec with Matchers with BeforeAndAfterAll with OptionValues with Inside {
 
     protected def getRndSuffix = scala.util.Random.alphanumeric.take(5).mkString.toLowerCase
 
+    protected def assertNotEmpty(cursor: Cursor): Unit = {
+        cursor.hasNext should be (true)
+    }
+
     protected def assertCursor(expected: Any, actual: Cursor): Unit = {
-        actual.hasNext should be (true)
-        actual.next() should be (expected)
+        assertNotEmpty(actual)
+        actual.next() should equal (expected)
     }
 
 }
