c849d5b37678ba58269e19c3251b051f9018fc0e
Eugene Janusov <esycat@gmail.com>
2013-11-19 00:39:16 +1100

    Testing date-time support.

--- src/test/scala/com/esyfur/rql/test/DateTimeSpec.scala
+++ src/test/scala/com/esyfur/rql/test/DateTimeSpec.scala
@@ -12,36 +12,38 @@ class DateTimeSpec extends UnitSpec with ConnectionAndDatabase with Tolerance {
     private val timeZone = DateTimeZone.forOffsetHours(10)
 
     private def seconds(dateTime: DateTime): Long = dateTime.getMillis / 1000L
-    private def epochTime(document: Document): Long = document("epoch_time").asInstanceOf[Double].toLong
 
-    private def assertTime(dateTime: DateTime)(cursor: Cursor): Unit = {
+    private def assertTime(dateTime: DateTime, tolerance: Option[Long] = None)(cursor: Cursor): Unit = {
         assertNotEmpty(cursor)
 
         val document = cursor.next().asInstanceOf[Document]
+        val timeZone = document("timezone")
+        val epochTime = document("epoch_time").asInstanceOf[Double].toLong
 
-        epochTime(document) should equal (seconds(dateTime))
-        document("timezone") should be (getTzOffset(dateTime.getZone))
+        timeZone should be (getTzOffset(dateTime.getZone))
+
+        tolerance match {
+            case Some(tol) => epochTime should equal (seconds(dateTime) +- tol)
+            case None      => epochTime should equal (seconds(dateTime))
+        }
     }
 
     describe("RQL") {
         describe("should be able to create a time object") {
             it("that represents current time in UTC") {
-                val dateTime = currentTime
+                val dateTime = currentTime // current date-time with up to a minute error
                 val cursor = r.now().run
-                assertNotEmpty(cursor)
-
-                val document = cursor.next().asInstanceOf[Document]
-                document("timezone") should be (getTzOffset(dateTime.getZone))
-                epochTime(document) should equal (seconds(dateTime) +- 60)
+                assertTime(dateTime, Some(60L))(cursor)
             }
 
             it("for a specific date in UTC") {
-                val dateTime = currentTime.withTimeAtStartOfDay
+                val dateTime = currentTime.withTimeAtStartOfDay // current date at midnight
                 val cursor = r.time(dateTime.getYear, dateTime.getMonthOfYear, dateTime.getDayOfMonth).run
                 assertTime(dateTime)(cursor)
             }
 
             it("for a specific date in a given timezone") {
+                // current date at midnight converted to the given timezone
                 val dateTime = currentTime.withTimeAtStartOfDay.withZoneRetainFields(timeZone)
                 val cursor = r.time(
                     dateTime.getYear, dateTime.getMonthOfYear, dateTime.getDayOfMonth,
@@ -51,6 +53,7 @@ class DateTimeSpec extends UnitSpec with ConnectionAndDatabase with Tolerance {
             }
 
             it("for a specific date and time in a given timezone") {
+                // current date-time converted to the given timezone
                 val dateTime = currentTime.withZoneRetainFields(timeZone)
                 val cursor = r.time(
                     dateTime.getYear, dateTime.getMonthOfYear, dateTime.getDayOfMonth,
@@ -61,12 +64,13 @@ class DateTimeSpec extends UnitSpec with ConnectionAndDatabase with Tolerance {
             }
 
             it("based on seconds since epoch") {
-                val dateTime = currentTime
+                val dateTime = currentTime // current date-time
                 val cursor = r.epochTime(seconds(dateTime)).run
                 assertTime(dateTime)(cursor)
             }
 
             it("based on an ISO 8601 string") {
+                // current date-time converted to the given timezone
                 val dateTime = currentTime.withZoneRetainFields(timeZone)
                 val cursor = r.iso8601(dateTime.toString).run
                 assertTime(dateTime)(cursor)
@@ -88,12 +92,20 @@ class DateTimeSpec extends UnitSpec with ConnectionAndDatabase with Tolerance {
                     assertTime(expected)(cursor)
 
                 }
+
                 it("based only on its date") {
                     val dateTime = currentTime
                     val expected = dateTime.withTimeAtStartOfDay()
                     val cursor = r.iso8601(dateTime.toString).date().run
                     assertTime(expected)(cursor)
                 }
+            }
+
+            it("number of seconds elapsed since the beginning of the day") {
+                val dateTime = currentTime
+                val expected = dateTime.getSecondOfDay +- 0.5
+                val cursor = r.expr(dateTime).timeOfDay().run
+                assertCursor(expected, cursor)
             }
         }
     }
