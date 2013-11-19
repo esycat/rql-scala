3e27d4561a743318ceab9d5a47b43e9999b079af
Eugene Janusov <esycat@gmail.com>
2013-11-20 00:38:24 +1100

    UnitSpec#assertQuery()

--- src/test/scala/com/esyfur/rql/test/UnitSpec.scala
+++ src/test/scala/com/esyfur/rql/test/UnitSpec.scala
@@ -3,6 +3,7 @@ package com.esyfur.rql.test
 import org.scalatest.{FunSpec, Matchers, BeforeAndAfterAll, OptionValues, Inside}
 
 import com.esyfur.rql.Cursor
+import com.esyfur.rql.core.Query
 
 abstract class UnitSpec extends FunSpec with Matchers with BeforeAndAfterAll with OptionValues with Inside {
 
@@ -12,9 +13,15 @@ abstract class UnitSpec extends FunSpec with Matchers with BeforeAndAfterAll wit
         cursor.hasNext should be (true)
     }
 
-    protected def assertCursor(expected: Any, actual: Cursor): Unit = {
-        assertNotEmpty(actual)
-        actual.next() should equal (expected)
+    protected def assertCursor(expected: Any, cursor: Cursor): Unit = {
+        assertNotEmpty(cursor)
+        val actual = cursor.next()
+        actual should equal (expected)
+    }
+
+    protected def assertQuery(expected: Any)(query: Query): Unit = {
+        val cursor = query.run
+        assertCursor(expected, cursor)
     }
 
 }
